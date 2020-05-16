4b5028bd45bb912d48c23930ac0e0df9065f5180
huy.pq <huy.pq@brggroup.vn>
2020-05-16 15:22:53 +0700

    up code

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 const bodyParser = require('body-parser');
 const authRouter = require('./src/route/auth-router.js');
 const userRouter = require('./src/route/user-router.js');
-
+const process = require('process');
 const userController = require('./src/controller/user-controller.js');
 
 var app = express();
@@ -27,3 +27,12 @@ app.get('/test', function (req, resp) {
 var server = app.listen(689, function () {
     console.log('Server running ...');
 });
+
+process
+  .on('unhandledRejection', (reason, p) => {
+    console.error(reason, 'Unhandled Rejection at Promise', p);
+  })
+  .on('uncaughtException', err => {
+    console.error(err, 'Uncaught Exception thrown');
+    process.exit(1);
+  });
