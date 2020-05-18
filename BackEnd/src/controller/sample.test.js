fae8650ded782cc629565a6cff2c2d33a69e48cf
huy.pq <huy.pq@brggroup.vn>
2020-05-18 11:12:12 +0700

    up coe

--- BackEnd/src/controller/sample.test.js
+++ BackEnd/src/controller/sample.test.js
@@ -1,5 +1,8 @@
+const userRepo = require('../repo/user-repo.js');
+
 describe('Sample Test', () => {
-    it('should test that true=true', ()=> {
-        expect(true).toBe(true);
+    it('should test that true=true', async ()=> {
+        const cUser = await userRepo.createUser({ username: 'huy', password: 'test1234', email: 'phanqhuy@gmail.com' });
+        expect(cUser.username).toBe('huy');
     })
 })
